876736782a2d02cf86d0bb8d7ae1981b88f1fab8
Jakub Jaskuła <jjcreator8@gmail.com>
2020-10-01 19:39:36 +0200

    Updates

---
+++ precache-manifest.242758e33ee892b1ddecd78fa3f7d837.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f996f044634f5941ae1ba66cb4bdc4c2",
+    "url": "/testpage/index.html"
+  },
+  {
+    "revision": "1f9ee7cab63151068671",
+    "url": "/testpage/static/css/main.23b03004.chunk.css"
+  },
+  {
+    "revision": "3606245162bf6295338b",
+    "url": "/testpage/static/js/2.87639b33.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/testpage/static/js/2.87639b33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f9ee7cab63151068671",
+    "url": "/testpage/static/js/main.0e853861.chunk.js"
+  },
+  {
+    "revision": "0ea22e9ac75e4cee477c",
+    "url": "/testpage/static/js/runtime-main.30f81f7f.js"
+  },
+  {
+    "revision": "1a278d527667ce0090f58061679de8b3",
+    "url": "/testpage/static/media/face.1a278d52.png"
+  }
+]);
